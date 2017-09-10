a171df88af3da1aaddb7a381892597e14a50ebc8
Qudsia Ahmad <qudsiaahmad@Aimens-MacBook-Pro.local>
2017-09-10 02:24:28 -0700

    attempt to solve anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,22 @@
 #!/usr/bin/env ruby
 class Game
-  def initialize(str)
-    @word = str
+  def initialize(str,string2)
+    @word1 = str
+    @word2 = str2
   end
 
-  def word_split()
-    @word.split(//).reverse()
+  # def word_split()
+  #   @word1.split(//).reverse()
+  #   @word2.split(//).reverse()
+  # end
+
+  def compare_words?(string1,string2)
+    if (string1.split(//).sort()) === (string2.split(//).sort())
+      true
+    else
+      false
+    end
   end
+
+
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,8 +3,9 @@ require('anagram')
 # require('pry')
 describe('#word_split') do
 
-  it("splits a word into individual letters and reverses them") do
-    ruby = Game.new("ruby")
-    expect(ruby.word_split()).to(eq(["y","b","u","r"]))
+  it("splits 2 words into individual letters and reverses them") do
+    ruby = Game.new("ruby", "bury")
+    expect(ruby.word_split()).to(eq(["y","b","u","r"]), ["r","u","b","y"])
   end
+
 end
