7015ec2516d594fd62388cb098400ae1fa34f297
Qudsia Ahmad <qudsiaahmad@Aimens-MacBook-Pro.local>
2017-09-13 20:50:09 -0700

    passes first 7 tests.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,43 +4,33 @@ require('anagram')
 
 describe('#game') do
   it("counts the number of vowels in both words") do
-    expect(checkWord("aimen")).to(eq(3))
+    expect(check_word("aimen")).to(eq(3))
   end
 
   it("capitalizes the first letter of all words in a multiple word title") do
-    expect(checkWord("lllll")).to(eq("Please input a word!"))
+    expect(check_word("lllll")).to(eq("Please input a word!"))
   end
 
   it("compares two words by sorting them into alphabetical order") do
-    expect(anagram('ruby', 'bury')).to(eq(true))
+    expect(check_anagram('ruby', 'bury')).to(eq("This is an anagram."))
   end
 
   it("compares two words by sorting them into alphabetical order") do
-    expect(anagram('very', 'bury')).to(eq(false))
+    expect(check_anagram('very', 'bury')).to(eq(false))
   end
 
   it("Compares two words to see if they are anagrams.") do
     expect(caps_checker('tea', 'EAT')).to(eq("These words are anagrams."))
   end
 
-  it("Compares two words to see if they are anagrams.") do
-    expect(caps_checker('tEa', 'tree')).to(eq("These words are not anagrams."))
+  it("Compares two words to see if they are antigrams.") do
+    expect(antigram('hello', 'stays')).to(eq("This is an antigram."))
+  end
+
+  it("makes a sentence into a string and checks to see if it is a palindrome.") do
+    expect(sentence_check("I am HOME")).to(eq("emohmai"))
   end
 
 
+
 end
-  #
-  #
-  #
-  # it("compares two words by downcasing, sorting and alphabetizing") do
-  #     ruby2 = Game.new('tea', 'EAT')
-  #   expect(ruby2.ignore_caps?()).to(eq(true))
-  # end
-  #
-  # it('takes a string and checks for vowels') do
-  #   expect(ruby3.vowel_checker("brfg")).to(eq(false))
-  # end
-  #
-  #
-  #
-  # # it "takes 2 words and puts them in an array"
